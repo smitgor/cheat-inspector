40580de62803bc0dadc220b4a5c9f45901b874ff
smitgor <smitgor120@gmail.com>
2023-11-02 02:35:24 +0530

    updated readme

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "./header/header";
+import Header from "./Header/Header";
 import { Outlet } from "react-router-dom";
 
 export default function Layout() {
